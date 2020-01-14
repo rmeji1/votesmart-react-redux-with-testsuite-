9e5adbdd2b3fe09118deb7bfb4506a2b0fc5fff6
rmeji1 <rmeji1@unh.newhaven.edu>
2020-01-13 19:24:09 -0500

    added test to test user interaction with navigation controller

--- src/__tests__/VoteSmartNavigationContainer.test.js
+++ src/__tests__/VoteSmartNavigationContainer.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 // import { describe, test, expect } from '@testing-library/jest-core'
 import { MemoryRouter as Router } from 'react-router-dom'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import VoteSmartNavigationContainer from '../VoteSmartNavigationContainer'
 
 describe ('<VoteSmartNavigationContainer>', () => {
@@ -12,14 +12,18 @@ describe ('<VoteSmartNavigationContainer>', () => {
   })
 
   test('should have "Local" navlink', () => {
-    const { getByText } = render(<Router initialEntries={['/local']}><VoteSmartNavigationContainer /></Router>)
+    const { getByText } = render(<Router initialEntries={['/']}><VoteSmartNavigationContainer /></Router>)
     const button = getByText('Local')
+    expect(button.className).not.toMatch(/active/)
+    fireEvent.click(button)
     expect(button.className).toMatch(/active/)
   })
 
   test('should have "Federal" navlink', () => {
-    const { getByText } = render(<Router initialEntries={['/federal']}><VoteSmartNavigationContainer /></Router>)
+    const { getByText } = render(<Router initialEntries={['/']}><VoteSmartNavigationContainer /></Router>)
     const button = getByText('Federal')
+    expect(button.className).not.toMatch(/active/)
+    fireEvent.click(button)
     expect(button.className).toMatch(/active/)
   })
 })
